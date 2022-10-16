d7945a178b2f51f4e41657c08b448eec4d6b9aba
Jack Gimbert <jack.gimbert@coastalcloud.us>
2022-10-16 10:23:49 -0400

    initial commit

---
+++ server/routes/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+module.exports = router;
